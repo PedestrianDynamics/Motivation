47f6825ca45e43861e84a813bc794bd3e42c9720
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2024-03-26 11:00:49 +0100

    ignore some files

---
+++ check.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+PYTHON_FILES=$(git ls-files '*.py')
+
+if [ -z "$PYTHON_FILES" ]; then
+    echo "No Python files found to check."
+    exit 0
+fi
+
+run_tool() {
+    echo ">> $1..."
+    $1 $2 $PYTHON_FILES
+}
+
+# Static type checker
+run_tool mypy "--strict"
+
+# Fast, extensible Python linter
+run_tool ruff "check"
+
+# Sort imports
+run_tool isort "check"
+
+echo "All checks and formatting done."
